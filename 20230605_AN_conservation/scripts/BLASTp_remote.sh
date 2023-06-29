0f9cc358704c3d020d3b5c5076b898570ad1c1f4
Amalia <nabuurs.a@gmail.com>
2023-06-29 14:44:48 +0200

    add BLAST script

---
+++ 20230605_AN_conservation/scripts/BLASTp_remote.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#SBATCH --time=01:00:00      
+#SBATCH --gres=tmpspace:1G      
+#SBATCH --mem=1G     
+
+# node needed for executing apptainer, but due to using the -remote flag it will be submitted on the 
+# blast servers which means there are minimal resouces needed to run this job
+
+# apptainer executes command to perform remote blastp search on fasta file containing protein sequences
+# it outputs a tsv file containing only the proteins WITH hits
+
+apptainer exec -B /hpc/pmc_vanheesch:/hpc/pmc_vanheesch /hpc/local/Rocky8/pmc_vanheesch/singularity_images/ncbi_blast-2.14.0.sif \
+    blastp \
+        -db nr \
+        -query $1 \
+        -out $2 \
+        -remote \
+        -evalue 0.05 \
+        -max_target_seqs 100 \
+        -seg yes \
+        -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qseq sseq"
+
+# $1 = input fasta
+# $2 = output file from the blastp search
+
+# put header line on top of the output file
+
+sed '1i qseqid  sseqid  pident  length  mismatch    gapopen qstart  qend    sstart  send    evalue  bitscore    qseq    sseq' $2
+
+# script to iterate over results to find proteins that do have and that do not have results
+
+apptainer exec -B /hpc/pmc_vanheesch:/hpc/pmc_vanheesch /hpc/local/Rocky8/pmc_vanheesch/singularity_images/blast_parser_python-1.0.sif \
+    python3 /app/blastout_to_list.py \
+        -b $2 \
+        -f $1 \
+        -o $3
+
+# $3 = output file with the identifier and wether there is homology found for one or not
