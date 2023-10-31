7ce94d46a677a53e6d2aae9eaf9237bc163bd32a
Amalia <nabuurs.a@gmail.com>
2023-10-31 09:24:04 +0100

    extra no parse script

---
+++ 20230503_AN_OmegaFold/scripts/omegafold_model2_noparse.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#SBATCH -p gpu
+#SBATCH -c 2
+#SBATCH --time=01:00:00      # All omegafold runs are between 1 and 5 minutes depending on the amount of residues adjust time accordingly
+#SBATCH --gres=tmpspace:10G
+#SBATCH --gpus-per-node=1
+#SBATCH --mem=15G            # Adjust to fit memory, around 1 GB per predicted structure
+
+apptainer exec --nv -B /hpc:/hpc /hpc/local/Rocky8/pmc_vanheesch/singularity_images/omegafold-1.0.0.sif omegafold --weights_file=/hpc/pmc_vanheesch/projects/Amalia/protein_prediction_project/20230503_AN_OmegaFold/model2.pt --model 2 $1 $2
+
+# 14-06-2023 AN
+# Script that executes OmegaFold and predicts 3D structure with it
+# $1 = input fasta file
+# $2 = output directory
+# run script with: 'sbatch omegafold_model1.sh input_fasta output_dir'
